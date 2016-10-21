294e6193d232a3edad2e1bc2d648e84fe20ad614
Mark Beukers <mark@afrotoss.com>
2016-10-21 22:55:48 +0800

    change run init to run bootstrap, more work on code

--- tools/feather.js
+++ tools/feather.js
@@ -4,14 +4,24 @@ const extract = require('extract-zip')
 const fs = require('fs')
 const colors = require('colors/safe')
 const R = require('ramda')
-var prompt = require('prompt')
+//var prompt = require('prompt')
 var ncp = require('ncp').ncp
 const spawn = require('child_process').spawn
 
 const tmpFolder = './tmp/'
+const skeletonSrc = './tools/skeletons/'
 const zipFile = tmpFolder + 'feather.latest.zip'
 const outFolder = tmpFolder + 'feather.bootstrap/'
 const latestPackage = 'https://github.com/Sitefinity/feather-packages/archive/master.zip'
+const tick = colors.green('✔')
+const cross = colors.red('✖')
+var installing = false
+const root = __dirname.replace('tools','')
+
+const ncpOptions = {transform: (read, write) => {
+  console.log('Copied ' + read.path.replace(__dirname, '') + ' -> ' + colors.green(write.path.replace(root, '')))
+  read.pipe(write)
+}}
 
 // Expected packages to install
 const Packages = [
@@ -20,53 +30,73 @@ const Packages = [
   {name: 'SemanticUI', path: 'feather-packages-master/community-packages/SemanticUI'},
   {name: 'Minimal', path: 'feather-packages-master/Minimal'}
 ]
+var ValidPackages = []
 
 // Setup tmp folder
 try { fs.mkdirSync(tmpFolder) } catch(e) {}
+process.stdin.setEncoding('utf8')
 
-// Grab Latest Package
+// Grab Latest Zip of feather-packages
+console.log('Downloading Feather Bootstrap..')
 request
   .get(latestPackage)
   .on('error', err => console.error(err))
-  .on('response', res => console.log('Feather Retrieved: ' + res.statusCode))
+  .on('response', res => console.log('Response: ' + colors.green(res.statusCode + ' OK') ))
   .pipe(fs.createWriteStream(zipFile))
   .on('finish', res => {
 
-    // Extract Zip
+    // Extract Zip to tmp folder
+    console.log('Download Complete, Extracting..')
     extract(zipFile, {dir: outFolder}, (err) => {
       if(err) return console.error(err)
 
-      // Validate Packages inside
-      console.log('\nPackages Extracted:')
-      const ValidPackages = Packages.map(VerifyPackage)
-      ValidPackages.forEach( p => {
-        console.log('- ' + p.name + ' ' + (p.valid ? colors.green('Valid') : colors.red('Invalid')))
+      // Validate Packages
+      ValidPackages = Packages.map(VerifyPackage)
+      console.log('\nFound '+colors.magenta(ValidPackages.length)+' Packages:')
+      ValidPackages.forEach( pkg => {
+        const pre = pkg.valid ? ' ' + tick + ' ' : ' ' + cross+ ' '
+        const post = pkg.installed ? ' - ' + colors.yellow('Installed!') : ''
+        console.log(pre + pkg.name + post)
       })
 
       // Ask User for package to install
-      prompt.start()
-      prompt.get({properties: {package: { description: 'Which packages to install?' }}}, (err, res) => {
-
-        if(err) return console.error(err)
-        const installPkg = R.find(R.propEq('name', res.package))(ValidPackages)
-        if(!installPkg) return console.error('Invalid package name: ' + res.package)
-        console.log('Installing ' + installPkg.name + '..')
-
-        // Install Package
-        ncp(outFolder + installPkg.path, './' + installPkg.name, (err) => {
-          if(err) return console.error(err)
-          console.log('Installed ' + installPkg.name + ' Successfully')
-
-          // Cleanup
-          spawn('rm', ['-R', 'tmp/'])
-            .on('close', code => console.log( code ? 'Error cleaning up: ' + code : 'Finished Cleaning up' ))
-        })
-
-      })
+      console.log('\nChoose a package to install: ')
+      return process.stdin.resume()
     })
   })
 
+// Package selector
+process.stdin.on('data', (text) => {
+
+  var installPkg = R.find(R.propEq('name', text.trim()))(ValidPackages)
+  if(!installPkg) return console.error(colors.red('Invalid package name: ') + text)
+  if(installing){ return true } else { installing = installPkg.name }
+  console.log('Installing ' + installPkg.name + ' Package..')
+
+  // Copy over the entire package
+  ncp(outFolder + installPkg.path, './' + installPkg.name, (err) => {
+    if(err) return console.error(err)
+    console.log('Installing ' + installPkg.name + ' Skeleton..')
+
+    // Copy over skeleton
+    ncp(skeletonSrc + installPkg.name, './src/', ncpOptions, (err) => {
+      if(err) return console.error(err)
+      
+      // Cleanup
+      spawn('rm', ['-R', 'tmp/'])
+        .on('close', code => {
+          console.log( code ? 'Error cleaning up: ' + code : 'Finished Installing' + installPkg.name )
+          process.stdin.pause()
+        })
+    })
+
+  })
+})
+
 const VerifyPackage = (pkg) => {
-  pkg.valid = true
+  pkg.valid = false
+  pkg.installed = false
+  if(fs.existsSync('./' + pkg.name)) { pkg.installed = true }
+  if(fs.existsSync(outFolder + pkg.path)){ pkg.valid = true }
   return pkg
 }
