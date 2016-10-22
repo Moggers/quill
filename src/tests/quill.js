949a2f94bc292cba786adc77c4c747c78b88c14b
Mark Beukers <mark@afrotoss.com>
2016-10-22 11:44:35 +0800

    Fixed all tests for bootstrap

--- src/tests/quill.js
+++ src/tests/quill.js
@@ -1,41 +1,91 @@
 'use strict'
+process.env.NODE_ENV = 'test'
 var chai = require('chai')
 var chaiAsPromised = require("chai-as-promised")
 chai.use(chaiAsPromised)
 var expect = chai.expect
+const exec = require('child_process').exec
+const Bootstrap = require('../../tools/feather.js')
+const fs = require('fs')
 
 describe('Quill', function(){
 
   describe('yarn bootstrap', function(){
 
-    const Bootstrap = require('../../tools/feather.js')
-    this.timeout(30000)
+    this.timeout(20000)
+
+    before('Delete all used folders', function(done){
+      exec('rm -R tmp/ Bootstrap/ Minimal/ Foundation/ SemanticUI/', err => {
+        done()
+      })
+    })
 
     it('can grab the latest package', function(){
-      var res = Bootstrap.getLatest()
-      expect(res).to.eventually.be.a('array')
-      expect(res).to.eventually.have.length(4)
-      return res
+      return Bootstrap.getLatest()
+        .then(function(x){
+          expect(x).to.be.a('array')
+          expect(x).to.have.length(4)
+        })
     })
 
-    it('can gather Bootstrap', function(){
-      //var res = 
+    it('can install Bootstrap', function(){
+      return Bootstrap.install(Bootstrap.Packages[0])
+        .then(function(res){
+          expect(res).to.be.true
+        })
     })
-    it('can gather Foundation')
-    it('can gather Minimal')
-    it('can gather SymantecUi')
-    it('can setup skeleton files')
-    it('can clean up after itself')
+    it('can gather Foundation', function(){
+      return Bootstrap.install(Bootstrap.Packages[1])
+        .then(function(res){
+          expect(res).to.be.true
+        })
+    })
+    it('can gather SemanticUI', function(){
+      return Bootstrap.install(Bootstrap.Packages[2])
+        .then(function(res){
+          expect(res).to.be.true
+        })
+    })
+    it('can gather Minimal', function(){
+      return Bootstrap.install(Bootstrap.Packages[3])
+        .then(function(res){
+          expect(res).to.be.true
+        })
+    })
+    it('can setup skeleton files', function(){
+      expect( fs.existsSync('./src/sass/styles.sass') ).to.be.true
+      expect( fs.existsSync('./src/js/app.js') ).to.be.true
+    })
+
+    it('can clean up after itself', function(){
+      return Bootstrap.cleanup()
+        .then(function(res){
+          expect(res).to.be.true
+        })
+    })
 
   })
 
   describe('yarn build', function(){
-    it('can produce a JS bundle')
-    it('can produce JS sourcemaps')
-    it('can produce a CSS bundle')
-    it('can produce CSS sourcemaps')
-    it('can optimize and produce source images')
-    it('can proudce spritesheets')
+    it('can produce a JS bundle', function(){
+      expect( fs.existsSync('./assets/js/app.js') ).to.be.true
+    })
+    it('can produce JS sourcemaps', function(){
+      expect( fs.existsSync('./assets/js/app.js.map') ).to.be.true
+    })
+    it('can produce a CSS bundle', function(){
+      expect( fs.existsSync('./assets/css/styles.css') ).to.be.true
+    })
+    it('can produce CSS sourcemaps', function(){
+      expect( fs.existsSync('./assets/css/styles.css.map') ).to.be.true
+    })
+    it('can optimize and produce source images', function(){
+      expect( fs.existsSync('./assets/img/favicon.png') ).to.be.true
+    })
+    it('can proudce spritesheets', function(){
+      expect( fs.existsSync('./assets/img/sprite.png') ).to.be.true
+      expect( fs.existsSync('./assets/img/sprite@2x.png') ).to.be.true
+    })
   })
 
   describe('yarn dev', function(){
--- tools/feather.js
+++ tools/feather.js
@@ -22,7 +22,7 @@ const beingLoaded = require.main.filename.indexOf('feather.js') > 0 // if being 
 
 // Log piped files
 const ncpOptions = {transform: (read, write) => {
-  console.log('Copied ' + read.path.replace(__dirname, '') + ' -> ' + colors.green(write.path.replace(root, '')))
+  log('Copied ' + read.path.replace(__dirname, '') + ' -> ' + colors.green(write.path.replace(root, '')))
   read.pipe(write)
 }}
 
@@ -33,6 +33,7 @@ const Packages = [
   {name: 'SemanticUI', path: 'feather-packages-master/community-packages/SemanticUI'},
   {name: 'Minimal', path: 'feather-packages-master/Minimal'}
 ]
+module.exports.Packages = Packages
 
 // Verify a package after it has been extracted
 const VerifyPackage = (pkg) => {
@@ -43,33 +44,38 @@ const VerifyPackage = (pkg) => {
   return pkg
 }
 
-// Setup tmp folder
-try { fs.mkdirSync(tmpFolder) } catch(e) {}
-process.stdin.setEncoding('utf8')
+// Logging
+function log(msg, extra){
+  if(process.env.NODE_ENV !== 'test') console.log(msg, extra)
+}
 
 module.exports.getLatest = function(){
+
+  // Setup tmp folder
+  try { fs.mkdirSync(tmpFolder) } catch(e) {}
+
   return new Promise((resolve, reject) => {
     // Grab Latest Zip of feather-packages
-    console.log('Downloading Feather Bootstrap..')
+    log('Downloading Feather Bootstrap..')
     request
       .get(latestPackage)
       .on('error', err => reject(err))
-      .on('response', res => console.log('Response: ' + colors.green(res.statusCode + ' OK') ))
+      .on('response', res => log('Response: ' + colors.green(res.statusCode + ' OK') ))
       .pipe(fs.createWriteStream(zipFile))
       .on('finish', res => {
 
         // Extract Zip to tmp folder
-        console.log('Download Complete, Extracting..')
+        log('Download Complete, Extracting..')
         extract(zipFile, {dir: outFolder}, (err) => {
           if(err) return reject(err)
 
           // Validate Packages
           const ValidPackages = Packages.map(VerifyPackage)
-          console.log('\nFound '+colors.magenta(ValidPackages.length)+' Packages:')
+          log('\nFound '+colors.magenta(ValidPackages.length)+' Packages:')
           ValidPackages.forEach( pkg => {
             const pre = pkg.valid ? ' ' + tick + ' ' : ' ' + cross+ ' '
             const post = pkg.installed ? ' - ' + colors.yellow('Installed!') : ''
-            console.log(pre + pkg.name + post)
+            log(pre + pkg.name + post)
           })
 
           // Ask User for package to install
@@ -82,17 +88,18 @@ module.exports.getLatest = function(){
 module.exports.install = function(installPkg){
   return new Promise( (resolve, reject) => {
     if(installing){ return true } else { installing = installPkg.name }
-    console.log('Installing ' + installPkg.name + ' Package..')
+    log('Installing ' + installPkg.name + ' Package..')
 
     // Copy over the entire package
     ncp(outFolder + installPkg.path, './' + installPkg.name, (err) => {
       if(err) return reject(err)
-      console.log('Installing ' + installPkg.name + ' Skeleton..')
+      log('Installing ' + installPkg.name + ' Skeleton..')
 
       // Copy over skeleton
       ncp(skeletonSrc + installPkg.name, './src/', ncpOptions, (err) => {
         if(err) return reject(err)
-        resolve()
+        installing = false
+        resolve(true)
       })
     })
   })
@@ -102,7 +109,7 @@ module.exports.cleanup = function(){
   return new Promise(resolve => {
     spawn('rm', ['-R', tmpFolder])
       .on('close', code => {
-        resolve()
+        resolve(true)
       })
   })
 }
@@ -113,14 +120,14 @@ module.exports.cli = function(){
   var ValidPackages = []
   process.stdin.on('data', (text) => {
     var installPkg = R.find(R.propEq('name', text.trim()))( ValidPackages )
-    if(!installPkg) return console.error(colors.red('Invalid package name: ') + text)
+    if(!installPkg) return log(colors.red('Invalid package name: ') + text)
     module.exports.install(installPkg)
   })
 
   module.exports.getLatest()
     .catch( err => new Error('error getting package: ' + err))
     .then( valid => {
-      console.log('Select a package to install:')
+      log('Select a package to install:')
       ValidPackages = valid
       process.stdin.pause()
     })
